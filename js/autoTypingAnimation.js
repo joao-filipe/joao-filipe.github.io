c18a56b0a619bc812468e4465d1baeab4df746dc
joao-filipe <44074186+joao-filipe@users.noreply.github.com>
2023-07-21 01:52:41 +0100

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am João Filipe </span> I'm from Chaves. Glad to see you on the site. <p> I'm Tutor as actual work so I work with kids and teaching them 			in - History, Portuguese, English, Spanish, chemistry...really like my work... . <p> I also do other things like Design and Multimedia with open 			tools Software like Gimp, Openshot, Blender.<br><p>I'm also a tecno-freak of electronic things - I love to test, repair-arm-realm old devices as like 			radios, old boards... I have a little Hack mind  ... althought I'm not a TI hacker!<br><br> I have a degree in Leisure and Tourism, so this is my 			real Professional area.<br> I like read and write and I love listen podcasts about History or mystery, learn TI, play video games, Blood Bowl 			(Humans, Ogres, Orks, Skavens, are my preferable teams) ...  " 
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
